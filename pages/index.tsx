1885f74eaf6f09fb445ee0844f5aec29e3465d52
Tomas Birbe <tomas.birbe@gmail.com>
2022-01-19 22:31:53 -0300

    Add drawer with transitions and improve desktop view

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,25 @@ import Projects from './components/Projects/Projects';
 import TwitterClone from './components/Projects/TwitterClone/TwitterClone';
 import Calculator from './components/Projects/Calculator/Calculator';
 import Contact from './components/Contact/Contact';
+import { useState } from 'react';
 
 export default function Home() {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  function onOpen() {
+    setToggle(true);
+  }
+
+  function onClose() {
+    setToggle(false);
+  }
+
+  function isOpen() {
+    return toggle;
+  }
+
   return (
     <main className={styles.container}>
-      <button type="button" className={styles.menuButton}>
-        <span className={styles.menu} />
-      </button>
       <Intro />
       <About />
       <Techs />
@@ -22,41 +34,68 @@ export default function Home() {
         <Calculator />
       </Projects>
       <Contact />
-      {/* <div className={styles.drawer}>
-        <ul>
-          <li>
-            <a href="" className={styles.link}>
+      <button type="button" className={styles.menuButton} onClick={onOpen}>
+        <span className={styles.menu} />
+      </button>
+
+      <div
+        className={`${styles.drawer}  ${
+          isOpen() ? styles.drawerOpen : styles.drawerClose
+        }`}
+      >
+        <ul className={styles.links}>
+          <li onClick={onClose}>
+            <a href="#intro" className={styles.link}>
               Introduccion
             </a>
           </li>
-          <li>
-            <a href="" className={styles.link}>
+          <li onClick={onClose}>
+            <a href="#about-me" className={styles.link}>
               Sobre mi
             </a>
           </li>
-          <li>
-            <a href="" className={styles.link}>
+          <li onClick={onClose}>
+            <a href="#techs" className={styles.link}>
               Tecnologias
             </a>
           </li>
-          <li>
+          <li onClick={onClose}>
             <a href="" className={styles.link}>
               Proyectos
             </a>
-            <ul>
-              <li>
-                <a href="">Twitter Clone</a>
+            <ul className={styles.links}>
+              <li onClick={onClose}>
+                <a href="#twitter-clone">Twitter Clone</a>
               </li>
-              <li>
-                <a href="">Calculadora</a>
+              <li onClick={onClose}>
+                <a href="#calculator">Calculadora</a>
               </li>
             </ul>
           </li>
-          <li>
-            <a href="">Contacto</a>
+          <li onClick={onClose}>
+            <a href="#contact">Contacto</a>
           </li>
         </ul>
-      </div> */}
+        <div className={styles.divider} />
+        <div className={styles.icons}>
+          <a href="mailto: tomas.birbe@gmail.com">
+            <img className={styles.email} src="icons/email.svg" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/tomas-birbe/" target="_blank">
+            <img
+              className={styles.linkedin}
+              src="icons/white-linkedin.svg"
+              alt=""
+            />
+          </a>
+          <a href="https://github.com/tomasbirbe" target="_blank">
+            <img className={styles.github} src="icons/github.svg" alt="" />
+          </a>
+        </div>
+        <button className={styles.backButton} onClick={onClose}>
+          <img src="icons/back.svg" alt="" className={styles.back} />
+        </button>
+      </div>
     </main>
   );
 }
