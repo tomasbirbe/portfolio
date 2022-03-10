3147e5e95450797c0f50bb91a63aa1cfb2fa85a9
Tomas Birbe <tomas.birbe@gmail.com>
2022-03-10 10:46:01 -0300

    Add small keeper project

--- src/components/Projects/SmallKeeper/SmallKeeper.tsx
+++ src/components/Projects/SmallKeeper/SmallKeeper.tsx
@@ -3,34 +3,48 @@ import Link from 'next/link';
 
 import { DownArrow } from '../../Icons';
 
-const Calculator = () => {
+const SmallKeeper = () => {
   return (
-    <section className="page px-8" id="calculator">
-      <article className="flex flex-row flex-row-reverse text-center md:gap-12 xl:gap-[8em] items-center justify-between">
+    <section className="page px-8" id="small-keeper">
+      <article className="flex flex-row text-center md:gap-12 xl:gap-[8em] items-center justify-between">
+        <Link href="https://tb-small-keeper.vercel.app/">
+          <a
+            className="hidden hover:scale-110 transition-all hover:shadow-xl hover: md:block md:w-[350px] md:h-[350px] lg:w-[500px] xl:w-[600px] relative"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <Image
+              alt="Vista previa del proyecto Small Keeper"
+              className="object-cover rounded-2xl w-[200px]"
+              layout="fill"
+              loading="lazy"
+              src="/small-keeper.webp"
+            />
+          </a>
+        </Link>
         <div className="flex h-full flex-col gap-8 items-center justify-between">
           <h3>
-            <Link href="https://tb-calculator.vercel.app/">
+            <Link href="https://tb-small-keeper.vercel.app/">
               <a
-                aria-label="Ir al proyecto Calculadora"
+                aria-label="Ir al proyecto Small-Keeper"
                 className="hover:bg-white hover:text-black p-2 rounded transition-all"
                 rel="noreferrer noopener"
                 target="_blank"
               >
-                Calculadora
+                Small Keeper
               </a>
             </Link>
           </h3>
           <div className="max-w-[370px] min-w-[250px]">
-            Este proyecto es una calculadora que puede guardar y mostrar valores definidos por el
-            usuario. Uno de los primeros proyectos que me ayudo a ganar experiencia con CSS Grid,
-            React y LocalStorage.
+            Este proyecto es un gestor de claves en donde podes guardar todas las claves que
+            quieras. Utiliza IndexDB a modo de demostracion.
           </div>
           <div className="flex gap-3 flex-wrap w-[150px]">
             <Image
-              alt="Logo de Styled Components"
+              alt="Logo de ChakraUI"
               height={40}
               loading="lazy"
-              src="/icons/styled-components.svg"
+              src="/icons/chakra.svg"
               width={40}
             />
             <Image
@@ -50,33 +64,18 @@ const Calculator = () => {
             <Image alt="Logo de CSS" height={40} loading="lazy" src="/icons/css.svg" width={40} />
             <Image alt="Logo de HTML" height={40} loading="lazy" src="/icons/html.svg" width={40} />
             <Image
-              alt="Logo de Styled Components"
+              alt="Logo de TypeScript"
               height={40}
               loading="lazy"
-              src="/icons/js.svg"
+              src="/icons/ts.svg"
               width={40}
             />
           </div>
         </div>
-        <Link href="https://tb-calculator.vercel.app/">
-          <a
-            className="hidden hover:scale-110 transition-all hover:shadow-xl hover: md:block md:w-[350px] md:h-[350px] lg:w-[500px] xl:w-[600px] relative"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <Image
-              alt="Vista previa del proyecto Calculator App"
-              className="object-cover rounded-2xl w-[200px]"
-              layout="fill"
-              loading="lazy"
-              src="/calculadora.webp"
-            />
-          </a>
-        </Link>
       </article>
-      <DownArrow href="#contact" />
+      <DownArrow href="#twitter-clone" />
     </section>
   );
 };
 
-export default Calculator;
+export default SmallKeeper;
